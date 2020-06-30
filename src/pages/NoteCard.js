c7d05a8115ceeb6157c63744809497299c202474
ashu <ayushgupta2305@gmail.com>
2020-06-30 18:16:20 +0530

    comment on Note added

--- src/pages/NoteCard.js
+++ src/pages/NoteCard.js
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+
 
 const NoteCard = (props) => {
- 
-   const {name, title, author, sem} = props.note
-         console.log(sem);
+  
+  const {name, title, author, sem, noteId} = props.note
+  //console.log(sem);
+  
+  
   return(
-   <div className="note-card-container"> 
-      <div className="note-card" >
-          <h2><b>Name : </b>{name} </h2>
-          <p><b>Title : </b>{title} </p>
-          <p><b>Author : </b> {author}</p>
-          <p><b>Semester : </b> {sem}</p>
+    <>
+      <div className="note-card-container"> 
+        <div className="note-card" >
+            <h2><b>Name : </b>{name} </h2>
+            <p><b>Title : </b>{title} </p>
+            <p><b>Author : </b> {author}</p>
+            <p><b>Semester : </b> {sem}</p>
 
-        <div className="clear"></div>
-     </div>  
-   </div>
-    )
+          <div className="clear"></div>
+        </div>  
+      </div>
+      
+    </>
+  )
 }
 
 export default NoteCard
--- src/pages/NoteViewer.js
+++ src/pages/NoteViewer.js
@@ -1,17 +1,116 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import NoteCard from './NoteCard'
 import { Button } from 'react-bootstrap'
 
+
+import axios from 'axios';
+
+import config from '../config.json';
+
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card'
+
 const NoteViewer = (props) => {
-  
+  const [data, setData] = useState({
+    noteId: ''
+  });
+  const [comments, setComments] = useState([]);
+
+  const [postComment, setpostComment] = useState('');
+
+  const { noteId} = props.note
+
+  const handleChange = e =>{
+    setpostComment(e.target.value);
+  };
+  console.log(data);
+  useEffect(() =>{
+    const getComment = async () => {
+      const data = {
+        noteId : noteId
+      }
+        try{
+          const res = await axios({
+            url: `${config.BASE}/getComments`,
+            method: "POST",
+            data : data
+          });
+            setComments([...res.data]);
+            console.log(res.data);
+        }
+        catch(error){
+            console.log(error);
+        }
+    };
+    getComment();
+  }, []);
+
+
+  const post = async() =>{
+    const postData = {
+      body: postComment,
+      noteId : noteId
+    }
+    try{
+      const res = await axios({
+        url: `${config.BASE}/commentOnNote`,
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${localStorage.FBIdToken}`
+      },
+        data : postData
+      });
+        
+        if(res.data){
+          console.log(res.data);
+          window.alert("Comment added successfull");
+        }
+    }
+    catch(error){
+        console.log(error);
+    }
+  }
+  console.log(comments);
+  const onSubmit = e =>{
+    e.preventDefault();
+    if(postComment !== ''){
+      post();
+    }else{
+      console.log("error");
+    }
+  }
   return(
-     <div>
-       <br></br><br></br><br></br>
-       <NoteCard note={props.note} />
-       <br></br>
-        <Button variant="primary" size="lg"   href={props.note.noteUrl} >Download</Button>{' '}
-        <Button variant="primary" size="lg" onClick={props.handleNoteGoBack}>Back</Button>{' '}
-     </div>
+    <>
+      <div>
+        <br></br><br></br><br></br>
+        <NoteCard note={props.note} />
+        <br></br>
+          <Button variant="primary" size="lg"   href={props.note.noteUrl} >Download</Button>{' '}
+          <Button variant="primary" size="lg" onClick={props.handleNoteGoBack}>Back</Button>{' '}
+      </div>
+      <br/><br/><br/><br/><br/>
+
+      <Container>
+        <h3>Comments: </h3>
+        <Row>
+          <Col>
+          
+            <input
+              type= "text"
+              name = 'body'
+              className="comment_input"
+              onChange = {handleChange}
+            />
+          </Col>
+          <Col>
+          <Button onClick={onSubmit} variant="outline-primary">Comment</Button>
+          </Col>
+        </Row>
+         
+      </Container>
+    </>
      )
 }
 
