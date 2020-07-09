41c642d8d8ca2a174776f4acc721f75ac7e91302
ashu <ayushgupta2305@gmail.com>
2020-07-09 22:49:11 +0530

    error handling

--- src/util/NavBar.jsx
+++ src/util/NavBar.jsx
@@ -12,7 +12,7 @@ const NavBar = () => {
     return(
         
         <>
-          <Navbar fixed="top" collapseOnSelect expand="lg" bg="primary" variant="dark">
+          <Navbar sticky="top" collapseOnSelect expand="lg" bg="primary" variant="dark">
             <Navbar.Brand href="/" className='Nav-head'>Notes Club</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
@@ -23,17 +23,17 @@ const NavBar = () => {
               </Nav>
               <Nav>
                 {state.isAuth && 
-                    <div className = 'Nav-item-br'><Nav.Item ><Link className='Nav-a' to = '/profile'>Profile</Link></Nav.Item></div>
-                }
-                {state.isAuth && 
-                    <div className = 'Nav-item-br'><Nav.Item ><Link className='Nav-a' to = '/logout'>Logout</Link></Nav.Item></div>
+                    <>
+                      <div className = 'Nav-item-br'><Nav.Item ><Link className='Nav-a' to = '/profile'>Profile</Link></Nav.Item></div>
+                      <div className = 'Nav-item-br'><Nav.Item ><Link className='Nav-a' to = '/logout'>Logout</Link></Nav.Item></div>
+                    </>
                 }
         
                 {!state.isAuth && 
+                  <>
                     <div className = 'Nav-item-br'><Nav.Item ><Link className='Nav-a' to = '/login'>Login</Link></Nav.Item></div>
-                }
-                {!state.isAuth && 
                     <div className = 'Nav-item-br'><Nav.Item ><Link className='Nav-a' to = '/signup'>SignUp</Link></Nav.Item></div>      
+                  </>
                 }
               
               </Nav>
